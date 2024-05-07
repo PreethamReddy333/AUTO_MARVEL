1ed0a423f94fd251f2675022d0fd422c590c23ab
PreethamReddy333 <81985981+PreethamReddy333@users.noreply.github.com>
2024-05-07 07:39:40 +0530

    main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ st.title('Welcome to :blue[_AutoMarvel_]')
 st.header("login or signup", divider="rainbow")
 
 if st.button('login'):
-    st.page_link('/home/preethamreddy/vscode/AUTOMARVEL/pages/loginpage.py')
+    st.page_link('AUTO_MARVEL/loginpage.py')
 if st.button('sign up'):
-    st.page_link('/home/preethamreddy/vscode/AUTOMARVEL/pages/signuppage.py')
+    st.page_link('AUTO_MARVEL/signuppage.py')
 
 
 

7e538a3cb958310c73a785ecff93c6de2d69a094
PreethamReddy333 <81985981+PreethamReddy333@users.noreply.github.com>
2024-05-07 07:31:49 +0530

    Add files via upload

---
+++ signuppage.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+LoginData = {"Preetham9070":"123456789Reddy", "1":"1"} 
+Dictnames = {"Preetham9070":"Preetham", "1":"1"}
+
+st.title("Sign up")
+a = st.text_input("name", key = 3)
+b = st.text_input("Enter Username👇", key=332)
+c = st.text_input("Enter password👇", key=423)
+
+if st.button("sign up"):
+    Dictnames[b] = a
+    LoginData[b] = c
+    st.page_link('AUTOMARVEL/pages/loginpage.py')
+
